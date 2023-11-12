4c36f5f06add477680c15a23ec0c9b0c3973214a
RITIK kumar <113612796+Ritikkumar993@users.noreply.github.com>
2023-11-12 17:29:17 +0530

    Reema.cpp

--- CSE-205 DSA/Unit-6/Lecture-37/Reema.cpp
+++ CSE-205 DSA/Unit-6/Lecture-37/Reema.cpp
@@ -1,71 +1,76 @@
-// ONly 3 test case running
-
+//all testcase is running
 #include <iostream>
 #include <vector>
-
+#include <string>
 using namespace std;
 
 const int INF = 1e7;
 
-void printPath(vector<vector<int>>& next, int start, int dest) {
-    cout << "Shortest path from Earth to planet " << dest << ": ";
-    if (next[start][dest] == -1) {
-        cout << start ;
-        if(dest>0){
-            cout<< " -> " << dest;
-        }
-    } else {
-        cout << start;
-        int current = start;
-        while (current != dest) {
-            cout << " -> " << next[current][dest];
-            current = next[current][dest];
-        }
-        cout << " -> " << dest;
+void printPath(int src, int dest, vector<vector<int>>& next) {
+    if (next[src][dest] == -1) {
+        cout << src;
+        return;
     }
-    cout << endl;
+
+    cout << src << " -> ";
+    while (src != dest) {
+        src = next[src][dest];
+        cout << src;
+        if (src != dest) {
+            cout << " -> ";
+        }
+    }
 }
 
-void floydWarshall(vector<vector<int>>& graph, int V, int dest) {
-    vector<vector<int>> dist = graph;
+void floydWarshall(int V, vector<vector<int>>& graph, int dest) {
+    vector<vector<int>> dist(V, vector<int>(V, 0));
     vector<vector<int>> next(V, vector<int>(V, -1));
 
-    // Applying Floyd-Warshall Algorithm
+    // Initialize distance and next arrays
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            dist[i][j] = graph[i][j];
+            if (graph[i][j] != INF && i != j) {
+                next[i][j] = j;
+            }
+        }
+    }
+
+    // Apply Floyd-Warshall algorithm
     for (int k = 0; k < V; ++k) {
         for (int i = 0; i < V; ++i) {
             for (int j = 0; j < V; ++j) {
-                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][j] > dist[i][k] + dist[k][j]) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
                     dist[i][j] = dist[i][k] + dist[k][j];
-                    next[i][j] = k;
+                    next[i][j] = next[i][k];
                 }
             }
         }
     }
 
     // Print the shortest path from Earth to the destination planet
-    printPath(next, 0, dest);
+    cout << "Shortest path from Earth to planet " << dest << ": ";
+    printPath(0, dest, next);
 }
 
 int main() {
     int V;
-    // cout << "Enter the number of planets in the galaxy (V): ";
     cin >> V;
 
-    // Input distance matrix
     vector<vector<int>> graph(V, vector<int>(V));
-    // cout << "Enter the distance matrix (0 to 10^7):\n";
     for (int i = 0; i < V; ++i) {
         for (int j = 0; j < V; ++j) {
             cin >> graph[i][j];
+            if (graph[i][j] == 0 && i != j) {
+                graph[i][j] = INF;
+            }
         }
     }
 
     int dest;
-    // cout << "Enter the index of the destination planet: ";
     cin >> dest;
 
-    // Apply Floyd-Warshall algorithm and print the result
-    floydWarshall(graph, V, dest);
+    floydWarshall(V, graph, dest);
 
     return 0;
 }
